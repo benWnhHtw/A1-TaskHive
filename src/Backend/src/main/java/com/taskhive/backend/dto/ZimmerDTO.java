8ca21c36cbed3741c05d661abc8cf020616ccf0a
Sirri-M <s89174@htw-dresden.de>
2026-06-08 20:27:05 +0200

    add ZimmerDTO.java

--- src/Backend/src/main/java/com/taskhive/backend/dto/ZimmerDTO.java
+++ src/Backend/src/main/java/com/taskhive/backend/dto/ZimmerDTO.java
@@ -7,4 +7,35 @@ public class ZimmerDTO {
     private Long personId;
     private Long wgNr;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getZimmerNr() {
+        return zimmerNr;
+    }
+
+    public void setZimmerNr(Integer zimmerNr) {
+        this.zimmerNr = zimmerNr;
+    }
+
+    public Long getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(Long personId) {
+        this.personId = personId;
+    }
+
+    public Long getWgNr() {
+        return wgNr;
+    }
+
+    public void setWgNr(Long wgNr) {
+        this.wgNr = wgNr;
+    }
 }
