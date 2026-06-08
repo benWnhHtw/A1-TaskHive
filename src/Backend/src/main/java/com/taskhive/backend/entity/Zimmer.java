648a4ce68c7a57afccc1c7405421e6cabf94ce23
Sirri-M <s89174@htw-dresden.de>
2026-06-08 20:25:44 +0200

    add Zimmer entity

--- src/Backend/src/main/java/com/taskhive/backend/entity/Zimmer.java
+++ src/Backend/src/main/java/com/taskhive/backend/entity/Zimmer.java
@@ -1,10 +1,53 @@
 package com.taskhive.backend.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "zimmer")
 public class Zimmer {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Integer zimmerNr;
-    private Long personId;
-    private Long WGNr;
 
+    @Column(name = "zimmer_nr")
+    private Integer zimmerNr;
+
+    @Column(name = "person_id")
+    private Long personId;
+
+    @Column(name = "wg_nr")
+    private Long wgNr;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getZimmerNr() {
+        return zimmerNr;
+    }
+
+    public void setZimmerNr(Integer zimmerNr) {
+        this.zimmerNr = zimmerNr;
+    }
+
+    public Long getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(Long personId) {
+        this.personId = personId;
+    }
+
+    public Long getWgNr() {
+        return wgNr;
+    }
+
+    public void setWgNr(Long wgNr) {
+        this.wgNr = wgNr;
+    }
 }
