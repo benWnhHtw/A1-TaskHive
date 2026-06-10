315b6c9c6758862a4c278069b57faf2eb769ce5b
Sirri-M <s89174@htw-dresden.de>
2026-06-10 21:56:38 +0200

    use lombok in Zimmer entity

--- src/Backend/src/main/java/com/taskhive/backend/dto/ZimmerDTO.java
+++ src/Backend/src/main/java/com/taskhive/backend/dto/ZimmerDTO.java
@@ -1,41 +1,25 @@
-package com.taskhive.backend.dto;
+package com.taskhive.backend.entity;
 
-public class ZimmerDTO {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+@Entity
+@Table(name = "zimmer")
+public class Zimmer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "zimmer_nr")
     private Integer zimmerNr;
+
+    @Column(name = "person_id")
     private Long personId;
+
+    @Column(name = "wg_nr")
     private Long wgNr;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Integer getZimmerNr() {
-        return zimmerNr;
-    }
-
-    public void setZimmerNr(Integer zimmerNr) {
-        this.zimmerNr = zimmerNr;
-    }
-
-    public Long getPersonId() {
-        return personId;
-    }
-
-    public void setPersonId(Long personId) {
-        this.personId = personId;
-    }
-
-    public Long getWgNr() {
-        return wgNr;
-    }
-
-    public void setWgNr(Long wgNr) {
-        this.wgNr = wgNr;
-    }
 }
