3f6668d6b986b9cddd161c9ac5acfea27c81944f
Sirri-M <s89174@htw-dresden.de>
2026-06-10 21:58:12 +0200

    use lombok value in ZimmerDTO

--- src/Backend/src/main/java/com/taskhive/backend/dto/ZimmerDTO.java
+++ src/Backend/src/main/java/com/taskhive/backend/dto/ZimmerDTO.java
@@ -1,25 +1,12 @@
-package com.taskhive.backend.entity;
+package com.taskhive.backend.dto;
 
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Value;
 
-@Getter
-@Setter
-@Entity
-@Table(name = "zimmer")
-public class Zimmer {
+@Value
+public class ZimmerDTO {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(name = "zimmer_nr")
-    private Integer zimmerNr;
-
-    @Column(name = "person_id")
-    private Long personId;
-
-    @Column(name = "wg_nr")
-    private Long wgNr;
+    Long id;
+    Integer zimmerNr;
+    Long personId;
+    Long wgNr;
 }
